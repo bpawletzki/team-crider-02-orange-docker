4e5fccdfa5fd0bc5796d03236486c1ff46e49bb2
Laura VanSchepen <lcappel@student.cscc.edu>
2021-11-17 21:56:53 -0500

    notes

--- public/inventoryAction.php
+++ public/inventoryAction.php
@@ -8,6 +8,7 @@ if (!empty($_POST) && $_POST['action'] == 'edit' && $_POST['id']) {
     if (isset($_POST['name'])) {
         $updateField .= "name='" . $_POST['name'] . "'";
         $updateField .= ", code='" . $_POST['code'] . "'";
+        $updateField .= ", category='" . $_POST['category'] . "'";
         $updateField .= ", image='" . $_POST['image'] . "'";
         $updateField .= ", price='" . $_POST['price'] . "'";
         $updateField .= ", description='" . $_POST['description'] . "'";
@@ -47,6 +48,7 @@ if (!empty($_POST) && $_POST['action'] == 'delete' && $_POST['id']) {
 if (!empty($_POST) && $_POST['action'] == 'list') {
     if (empty($_SESSION['searchName'])) {$_SESSION['searchName'] = "%";};
     if (empty($_SESSION['searchCode'])) {$_SESSION['searchCode'] = "%";};
+    if (empty($_SESSION['searchCategory'])) {$_SESSION['searchCategory'] = "%";};
     if (empty($_SESSION['searchImage'])) {$_SESSION['searchImage'] = "%";};
     if (empty($_SESSION['searchPrice'])) {$_SESSION['searchPrice'] = "%";};
     if (empty($_SESSION['searchDescription'])) {$_SESSION['searchDescription'] = "%";};
@@ -54,10 +56,11 @@ if (!empty($_POST) && $_POST['action'] == 'list') {
     $searchField = '';
     $searchField .= "name LIKE '" . $_SESSION['searchName'] . "' ";
     $searchField .= "AND code LIKE '" . $_SESSION['searchCode'] . "' ";
+    $searchField .= "AND category LIKE '" . $_SESSION['searchCategory'] . "' ";
     $searchField .= "AND image LIKE '" . $_SESSION['searchImage'] . "' ";
     $searchField .= "AND price LIKE '" . $_SESSION['searchPrice'] . "' ";
     $searchField .= "AND description LIKE '" . $_SESSION['searchDescription'] . "' ";
-    $sqlQuery = "SELECT id, name, code, image, price, description FROM product WHERE " . $searchField;
+    $sqlQuery = "SELECT id, name, code, category, image, price, description FROM product WHERE " . $searchField;
     $resultSet = mysqli_query($link, $sqlQuery);
 ?>
 
@@ -68,6 +71,7 @@ if (!empty($_POST) && $_POST['action'] == 'list') {
                 <th>Id</th>
                 <th>Name</th>
                 <th>Code</th>
+                <th>Category</th>
                 <th>Image</th>
                 <th>Price</th>
                 <th>Description</th>
@@ -79,6 +83,7 @@ if (!empty($_POST) && $_POST['action'] == 'list') {
                     <td data-input="searchId"><?php echo $product['id']; ?></td>
                     <td data-input="searchName"><?php echo $product['name']; ?></td>
                     <td data-input="searchCode"><?php echo $product['code']; ?></td>
+                    <td data-input="searchCategory"><?php echo $product['category']; ?></td>
                     <td data-input="searchImage"><?php echo $product['image']; ?></td>
                     <td data-input="searchPrice"><?php echo $product['price']; ?></td>
                     <td data-input="searchDescription"><?php echo $product['description']; ?></td>

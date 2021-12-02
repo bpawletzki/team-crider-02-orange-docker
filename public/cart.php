2597067fb67509fc9bfde852a69f113b56d86392
Nick Ramey <nramey3@student.cscc.edu>
2021-12-01 23:27:39 -0500

    Changed quantity fields to only be numeric, updated formatting for cart and receipt.

--- public/cart.php
+++ public/cart.php
@@ -7,11 +7,11 @@ if (!empty($_GET["action"])) {
 		case "add":
 			if (!empty($_POST["quantity"])) {
 				$productByCode = $db_handle->runQuery("SELECT * FROM product WHERE code='" . $_GET["code"] . "'");
-				$k = $productByCode[0]["code"] . $_POST["creamer-options"] . $_POST["sweetener-options"] . $_POST["syrup-options"];
-				$itemArray = array($k => array('name' => $productByCode[0]["name"], 'code' => $productByCode[0]["code"], 'quantity' => $_POST["quantity"], 'price' => $productByCode[0]["price"], 'image' => $productByCode[0]["image"], 'id' => $productByCode[0]["id"], 'creamer' => $_POST["creamer-options"], 'sweetener' => $_POST["sweetener-options"], 'syrup' => $_POST["syrup-options"], 'pumps' => $_POST["pumps"]));
+				$k = $productByCode[0]["code"] . $_POST["creamer-options"] . $_POST["sweetener-options"] . $_POST["syrup-options"] . $_POST["pumps"];
+				$itemArray = array($k => array('name' => $productByCode[0]["name"], 'code' => $productByCode[0]["code"], 'category' => $productByCode[0]["category"], 'quantity' => $_POST["quantity"], 'price' => $productByCode[0]["price"], 'image' => $productByCode[0]["image"], 'id' => $productByCode[0]["id"], 'creamer' => $_POST["creamer-options"], 'sweetener' => $_POST["sweetener-options"], 'syrup' => $_POST["syrup-options"], 'pumps' => $_POST["pumps"]));
 				// echo $itemArray;
 				if (!empty($_SESSION["cart_item"])) {
-					$resultsArraySearch = preg_grep("/.*?" . $productByCode[0]["code"] . $_POST["creamer-options"] . $_POST["sweetener-options"] . $_POST["syrup-options"] . "*?./i", array_keys($_SESSION["cart_item"]));
+					$resultsArraySearch = preg_grep("/.*?" . $productByCode[0]["code"] . $_POST["creamer-options"] . $_POST["sweetener-options"] . $_POST["syrup-options"] . $_POST["pumps"] . "*?./i", array_keys($_SESSION["cart_item"]));
 					if ($resultsArraySearch) {
 						if (empty($_SESSION["cart_item"][$k]["quantity"])) {
 							$_SESSION["cart_item"][$k]["quantity"] = 0;
@@ -102,13 +102,19 @@ if (!empty($_GET["action"])) {
 							</td>
 							<td> <?php echo $item["creamer"]; ?></td><!-- the creamer options are supposed to print, the variable is added at the end of line 10-->
 							<td> <?php echo $item["sweetener"]; ?></td><!-- the sweetener options are supposed to print, the variable is added at the end of line 10-->
-							<td> <?php echo $item["syrup"]; ?>
-							<?php echo $item["pumps"]; ?></td><!-- the syrup options are supposed to print, the variable is added at the end of line 10-->
+							<?php 
+								if($item["pumps"] != 0) {
+									echo "<td>" . $item['pumps'] . " pumps of " . $item['syrup'] . "</td>"; // the syrup options are supposed to print, the variable is added at the end of line 10-->
+								} elseif ($item["category"] == "bakery") {
+									echo "<td>" . $item["pumps"] . "</td>";
+								} else {
+                                    echo "<td> None </td>";
+                                }?>
 							<td><?php echo $item["code"]; ?></td>
 							<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
 							<td style="text-align:right;"><?php echo "$ " . $item["price"]; ?></td>
 							<td style="text-align:right;"><?php echo "$ " . number_format($item_price, 2); ?></td>
-							<td style="text-align:center;"><a href="cart.php?action=remove&code=<?php echo $item["code"].$item["creamer"].$item["sweetener"].$item["syrup"]; ?>" class="btnRemoveAction"><img src="./assets/img/icon-delete.png" alt="Remove Item" /></a></td>
+							<td style="text-align:center;"><a href="cart.php?action=remove&code=<?php echo $item["code"].$item["creamer"].$item["sweetener"].$item["syrup"].$item["pumps"]; ?>" class="btnRemoveAction"><img src="./assets/img/icon-delete.png" alt="Remove Item" /></a></td>
 						</tr>
 					<?php
 						$total_quantity += $item["quantity"];

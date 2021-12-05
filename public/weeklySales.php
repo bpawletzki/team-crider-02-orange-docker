f55a361468388b61aa62642f03254f6b98f2287e
Brian Pawletzki <bpawletzki@student.cscc.edu>
2021-12-05 10:16:08 -0500

    weekly saves: initial page complete

--- public/weeklySales.php
+++ public/weeklySales.php
@@ -6,49 +6,8 @@ if (is_null($_SESSION["empLoggedin"]) || !($_SESSION["empLoggedin"])) {
     header("location: employeeLogin.php");
     exit;
 }
-
-if (!empty($_POST['name'])) {
-    $_SESSION['searchName'] = "%" . $_POST['name'] . "%";
-    $_SESSION['placeName'] = $_POST['name'];
-} else {
-    $_SESSION['searchName'] = "%";
-    $_SESSION['placeName'] = "Name";
-};
-if (!empty($_POST['code'])) {
-    $_SESSION['searchCode'] = "%" . $_POST['code'] . "%";
-    $_SESSION['placeCode'] = $_POST['code'];
-} else {
-    $_SESSION['searchCode'] = "%";
-    $_SESSION['placeCode'] = "Code";
-};
-if (!empty($_POST['category']) && $_POST['category']!="null") {
-    $_SESSION['searchCategory'] = "%" . $_POST['category'] . "%";
-    $_SESSION['placeCategory'] = $_POST['category'];
-} else {
-    $_SESSION['searchCategory'] = "%";
-    $_SESSION['placeCategory'] = "Category";
-};
-if (!empty($_POST['image'])) {
-    $_SESSION['searchImage'] = "%" . $_POST['image'] . "%";
-    $_SESSION['placeImage'] = $_POST['image'];
-} else {
-    $_SESSION['searchImage'] = "%";
-    $_SESSION['placeImage'] = "Image";
-};
-if (!empty($_POST['price'])) {
-    $_SESSION['searchPrice'] = "%" . $_POST['price'] . "%";
-    $_SESSION['placePrice'] = $_POST['price'];
-} else {
-    $_SESSION['searchPrice'] = "%";
-    $_SESSION['placePrice'] = "Price";
-};
-if (!empty($_POST['description'])) {
-    $_SESSION['searchDescription'] = "%" . $_POST['description'] . "%";
-    $_SESSION['placeDescription'] = $_POST['description'];
-} else {
-    $_SESSION['searchDescription'] = "%";
-    $_SESSION['placeDescription'] = "Description";
-};
+require_once("dbcontroller.php");
+$db_handle = new DBController();
 
 ?>
 <!DOCTYPE html>
@@ -63,117 +22,100 @@ if (!empty($_POST['description'])) {
 
 <body>
     <?php include('./components/nav.php') ?>
-
     <div id="inventory">
         <div class="bg-light border rounded border-light jumbotron py-5 px-4">
-            <h1 style="font-family: 'Abril Fatface', serif;">Update Inventory</h1>
+            <h1 style="font-family: 'Abril Fatface', serif;">Weekly Report</h1>
             <p></p>
         </div>
     </div>
-    <?php
-
-    ?>
 
     <div class="container site-section" id="inventoryDetails">
-        <h1 style="font-family: 'Abril Fatface', serif;">Update Inventory</h1>
-        <form method="post" action="inventoryUpdate.php">
-            <input type="text" id="searchName" class="search-key" name="name" placeholder="Name">
-            <input type="text" id="searchCode" class="search-key" name="code" placeholder="Code">
-            <select name="category" id="searchCategory" class="search-key" placeholder="Category">
-                <option value="null">Category</option>
-                <option value="hot">hot</option>
-                <option value="iced">iced</option>
-                <option value="frozen">frozen</option>
-                <option value="bakery">bakery</option>
-            </select>
-            <input type="text" id="searchImage" class="search-key" name="image" placeholder="Image">
-            <input type="text" id="searchPrice" class="search-key" name="price" placeholder="Price">
-            <input type="text" id="searchDescription" class="search-key" name="description" placeholder="Description">
-            <input type="submit" id="searchButton" name="action" value="Search">
-            <br>
-        </form>
+        <h1 style="font-family: 'Abril Fatface', serif;">&nbsp;</h1>
         <div id="dataTable">
+          <table class="table table-hover table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Product</th>
+                        <th scope="col">Monday</th>
+                        <th scope="col">Tuesday</th>
+                        <th scope="col">Wednesday</th>
+                        <th scope="col">Thursday</th>
+                        <th scope="col">Friday</th>
+                        <th scope="col">Saturday</th>
+                        <th scope="col">Sunday</th>
+                        <th scope="col">Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $product_array = $db_handle->runQuery("SELECT i.id, i.name,
+                    SUM(CASE WHEN vdayOfWeek = 0 THEN coalesce(total,0) END) \"0\",
+                    SUM(CASE WHEN vdayOfWeek = 1 THEN coalesce(total,0) END) \"1\",
+                    SUM(CASE WHEN vdayOfWeek = 2 THEN coalesce(total,0) END) \"2\",
+                    SUM(CASE WHEN vdayOfWeek = 3 THEN coalesce(total,0) END) \"3\",
+                    SUM(CASE WHEN vdayOfWeek = 4 THEN coalesce(total,0) END) \"4\",
+                    SUM(CASE WHEN vdayOfWeek = 5 THEN coalesce(total,0) END) \"5\",
+                    SUM(CASE WHEN vdayOfWeek = 6 THEN coalesce(total,0) END) \"6\",
+                    sum(total) as productTotal
+                    FROM (select p.id, p.name, sum(cd.price)+sum(cd.pumps)*0.25 as total, weekday(c.checkoutTime) as vdayOfWeek
+                    FROM product as p
+                    LEFT JOIN checkoutDetail as cd ON p.id=cd.product_id
+                    LEFT JOIN checkout as c ON cd.checkout_id=c.id
+                    WHERE c.checkoutTime>DATE_ADD(c.checkoutTime, INTERVAL -1 WEEK) OR c.checkoutTime is Null
+                    GROUP BY p.id) as i
+                    GROUP BY i.id
+                    ORDER BY i.id
+                    ");
+                    $total0=0;
+                    $total1=0;
+                    $total2=0;
+                    $total3=0;
+                    $total4=0;
+                    $total5=0;
+                    $total6=0;
+                    $totalWeek=0;
 
+                    if (!empty($product_array)) {
+                        foreach ($product_array as $key => $value) {
+                    ?>
+                            <tr>
+                                <th scope="row"><?php echo $product_array[$key]["id"]; ?></th>
+                                <td><?php echo $product_array[$key]["name"]; ?></td>
+                                <td><?php if (($product_array[$key]["0"])!=0) {printf("$%.2f", $product_array[$key]["0"]);$total0=$product_array[$key]["0"]+$total0;}; ?></td>
+                                <td><?php if (($product_array[$key]["1"])!=0) {printf("$%.2f", $product_array[$key]["1"]);$total1=$product_array[$key]["1"]+$total1;}; ?></td>
+                                <td><?php if (($product_array[$key]["2"])!=0) {printf("$%.2f", $product_array[$key]["2"]);$total2=$product_array[$key]["2"]+$total2;}; ?></td>
+                                <td><?php if (($product_array[$key]["3"])!=0) {printf("$%.2f", $product_array[$key]["3"]);$total3=$product_array[$key]["3"]+$total3;}; ?></td>
+                                <td><?php if (($product_array[$key]["4"])!=0) {printf("$%.2f", $product_array[$key]["4"]);$total4=$product_array[$key]["4"]+$total4;}; ?></td>
+                                <td><?php if (($product_array[$key]["5"])!=0) {printf("$%.2f", $product_array[$key]["5"]);$total5=$product_array[$key]["5"]+$total5;}; ?></td>
+                                <td><?php if (($product_array[$key]["6"])!=0) {printf("$%.2f", $product_array[$key]["6"]);$total6=$product_array[$key]["6"]+$total6;}; ?></td>
+                                <td><?php if (($product_array[$key]["productTotal"])!=0) {printf("$%.2f", $product_array[$key]["productTotal"]);$totalWeek=$product_array[$key]["productTotal"]+$totalWeek;}; ?></td>
+                            </tr>
+                    <?php
+                        }
+                        ?>
+                        <tr>
+                        <th scope="row">Totals</th>
+                        <td>All Products</td>
+                        <td><?php if (($product_array[$key]["0"])!=0) {printf("$%.2f", $total0);}; ?></td>
+                        <td><?php if (($product_array[$key]["1"])!=0) {printf("$%.2f", $total1);}; ?></td>
+                        <td><?php if (($product_array[$key]["2"])!=0) {printf("$%.2f", $total2);}; ?></td>
+                        <td><?php if (($product_array[$key]["3"])!=0) {printf("$%.2f", $total3);}; ?></td>
+                        <td><?php if (($product_array[$key]["4"])!=0) {printf("$%.2f", $total4);}; ?></td>
+                        <td><?php if (($product_array[$key]["5"])!=0) {printf("$%.2f", $total5);}; ?></td>
+                        <td><?php if (($product_array[$key]["6"])!=0) {printf("$%.2f", $total6);}; ?></td>
+                        <td><?php if (($product_array[$key]["productTotal"])!=0) {printf("$%.2f", $totalWeek);}; ?></td>
+                       </tr>
+                    <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div><br>
     <div class="container site-section" id=responseStatusDisplay></div>
     <script type="text/javascript" src="./assets/js/jquery.min.js"></script>
     <script type="text/javascript" src="./assets/bootstrap/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="./assets/bootstrap/js/bootstable.js"></script>
-    <script type="text/javascript">
-        function myLoadTable() {
-            $('#searchButton').submit(
-                "inventoryAction.php", {
-                    action: "list"
-                });
-            $('#dataTable').load(
-                "inventoryAction.php", {
-                    action: "list"
-                },
-                function() {
-                    // https://github.com/t-edson/bootstable
-                    $('#editableTable').SetEditable({
-                        bootstrap: false,
-                        columnsEd: "1,2,3,4,5,6",
-                        columnsType: "text,text,text,select,text,text,text",
-                        columnsSelect:[{3: ["hot","iced","frozen","bakery"]}],
-                        onEdit: function(columnsEd) {
-                            var productId = columnsEd[0].childNodes[1].innerHTML;
-                            var productName = columnsEd[0].childNodes[3].innerHTML;
-                            var productCode = columnsEd[0].childNodes[5].innerHTML;
-                            var productCategory = columnsEd[0].childNodes[7].innerHTML;
-                            var productImage = columnsEd[0].childNodes[9].innerHTML;
-                            var productPrice = columnsEd[0].childNodes[11].innerHTML;
-                            var productDescription = columnsEd[0].childNodes[13].innerHTML;
-                            $.ajax({
-                                type: 'POST',
-                                url: "inventoryAction.php",
-                                dataType: "json",
-                                data: {
-                                    id: productId,
-                                    name: productName,
-                                    code: productCode,
-                                    category: productCategory,
-                                    image: productImage,
-                                    price: productPrice,
-                                    description: productDescription,
-                                    action: 'edit'
-                                },
-                                success: function(response) {
-                                    if (!response.status) {
-                                        $("#responseStatusDisplay").html(response.message)
-                                    }
-                                    myLoadTable();
-
-                                }
-                            });
-                        },
-                        onBeforeDelete: function(columnsEd) {
-                            var productId = columnsEd[0].childNodes[1].innerHTML;
-                            $.ajax({
-                                type: 'POST',
-                                url: "inventoryAction.php",
-                                dataType: "json",
-                                data: {
-                                    id: productId,
-                                    action: 'delete'
-                                },
-                                success: function(response) {
-                                    if (!response.status) {
-                                        $("#responseStatusDisplay").html("Unable to delete row. The product must be removed from all receipts before it can be removed.")
-                                    }
-                                    myLoadTable();
-                                },
-
-                            });
-                        },
-                    })
-                });
-        };
-        myLoadTable();
-    </script>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <?php include('./components/footer.php') ?>
 </body>
 
